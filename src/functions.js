bef2b7b9b18cd9e6ab15da153720dc97bb2ae96e
Felipe Martins <juninpernadecano@gmail.com>
2024-06-22 15:30:28 -0400

    fisrt commit

---
+++ src/functions.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicacao(5, 5));
+console.log(saudacao('Felipe'));
